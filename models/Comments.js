201d2dd302dc911138e0ad9de4a0d9c3d9a88c41
Garima Jyoti <jyotigarima2000@gmail.com>
2023-10-12 16:00:31 -0400

    Comment model added

---
+++ models/Comments.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    commenterName: {
+        type: String,
+        required: true,
+    },
+    commentText: {
+        type: String,
+        required: true,
+    },
+    creationDate: {
+        type: Date,
+        default: Date.now,
+    },
+    blogPost: {
+        type: Schema.Types.ObjectId,
+        ref: "Blog",
+    },
+});
+
+export default mongoose.model("Comment", commentSchema);
