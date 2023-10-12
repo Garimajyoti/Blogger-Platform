dc8c5070b33823a9941c9dd2de3dfbaa153e0dd4
Garima Jyoti <jyotigarima2000@gmail.com>
2023-10-12 01:43:31 -0400

    Get all blogs endpoint is added

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,7 +1,16 @@
 import Blog from "../models/Blog";
 
 export const getAllBlogs = async (request, response) => {
-
+    let allBlogs;
+    try {
+        allBlogs = await Blog.find();
+    } catch (err) {
+        console.log(err);
+    }
+    if (!allBlogs) {
+        return response.status(404).json({ message: "No blog posts found" });
+    }
+    return response.status(200).json({ allBlogs });
 };
 
 export const createBlog = async (request, response) => {
