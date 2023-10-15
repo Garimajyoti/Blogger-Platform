1316e9831cafd1f96863cee55ed29f2a6786816d
Garima Jyoti <jyotigarima2000@gmail.com>
2023-10-14 20:42:50 -0400

    Update comment endpoint is added

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -30,3 +30,25 @@ export const getCommentsByBlogPost = async (request, response) => {
         return response.status(500).json({ message: "Error fetching comments" });
     }
 };
+
+export const updateComment = async (request, response) => {
+    const commentId = request.params.id;
+    const { commentText } = request.body;
+
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(
+            commentId,
+            { commentText },
+            { new: true }
+        );
+
+        if (!updatedComment) {
+            return response.status(404).json({ message: "Comment cannot found" });
+        }
+
+        return response.status(200).json({ updatedComment });
+    } catch (err) {
+        console.log(err);
+        return response.status(500).json({ message: "Error updating the comment" });
+    }
+};
